d8170406122fc61fc273e5eb192ac793abc39979
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-10-19 22:25:58 -0500

    init commit

---
+++ subscriberlist/src/main/java/com/tts/subscriberlist/subscriber/subscriberRepository.java
@@ -0,0 +1,6 @@
+package com.tts.subscriberlist.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+public interface subscriberRepository extends CrudRepository<Subscriber, Long>{
+    
+}
